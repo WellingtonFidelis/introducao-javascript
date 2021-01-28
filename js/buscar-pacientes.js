78c5d5a85e93d1c8fb855674d9e251acdf09dc6d
WellingtonFidelis <wellingtonf20@gmail.com>
2021-01-27 23:05:25 -0300

    function to filter names on table and get api

---
+++ js/buscar-pacientes.js
@@ -0,0 +1,14 @@
+var buttonAdd = document.querySelector('#buscar-paciente');
+
+buttonAdd.addEventListener('click', function () {
+    // console.log('buscando pacientes');
+    var xhr = new XMLHttpRequest();
+
+    xhr.open('GET', 'https://api-pacientes.herokuapp.com/pacientes');
+
+    xhr.addEventListener('load', function () {
+        console.log(xhr.responseText);
+    });
+
+    xhr.send();
+});
--- js/filter.js
+++ js/filter.js
@@ -1,13 +1,27 @@
 var inputTableSearch = document.querySelector('#inputTableSearch');
 
 inputTableSearch.addEventListener('input', function(event){
-    this.value;
-
     var pacientes = document.querySelectorAll('.paciente');
 
-    pacientes.forEach(function(paciente, index){
-        var tdName = paciente.querySelector('.info-nome');
-        var name = tdName.textContent;
-        console.log(name);
-    });
+    if (this.value.length > 0) {
+        console.log('was clicked');
+        for (var i = 0; i < pacientes.length; i++) {
+            var paciente = pacientes[i];
+            var tdName = paciente.querySelector('.info-nome');
+            var name = tdName.textContent;
+            var expressao = new RegExp(this.value, 'i');
+            console.log(name);
+            // if (name != this.value) {
+            if (!expressao.test(name)) {
+                paciente.classList.add('invisible');
+            } else {
+                paciente.classList.remove('invisible');
+            } 
+        }
+    } else {
+        for (var i = 0; i < pacientes.length; i++) {
+            var paciente = pacientes[i];
+            paciente.classList.remove('invisible');
+        }
+    }
 });
